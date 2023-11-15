8fb8253dc80a3205c2a36bf4bff4acaf6354dcb5
Srinathmp <46724188+Srinathmp@users.noreply.github.com>
2023-11-15 13:20:44 +0530

    Update sonarqube.sh with command description
    Update sonarqube.sh with command description

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,1 +1,15 @@
  docker run -d --name sonarqube -p 9000:9000 -p 9092:9092 sonarqube
+
+ #This command is used to run a Docker container with SonarQube, a popular open-source platform for continuous inspection of code quality. Let's break down the command:
+
+#1. docker run: This command is used to create and start a new Docker container.
+
+#2. -d: This option stands for "detached" mode. It means the container runs in the background, and the terminal is not attached to the container's console. This is commonly used for long-running services.
+
+#3. --name sonarqube: This option specifies the name of the Docker container. In this case, it's named "sonarqube."
+
+#4. -p 9000:9000 -p 9092:9092: This option maps ports between the Docker container and the host system. SonarQube typically uses port 9000 for web access, and port 9092 for communication with scanners. With this option, the host's port 9000 is mapped to the container's port 9000, and the host's port 9092 is mapped to the container's port 9092.
+
+#5. sonarqube: This is the name of the Docker image to be used for creating the container. Docker will look for this image locally, and if it doesn't find it, it will attempt to pull it from the Docker Hub.
+
+#So, when you run this command, Docker will create a detached container named "sonarqube" based on the "sonarqube" image. The SonarQube web interface will be accessible on http://localhost:9000, and port 9092 will be available for communication with scanners. The -d flag ensures that the container runs in the background.
